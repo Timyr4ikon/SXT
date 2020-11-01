1a17acc0f3848cd9372486f633da1edb9c3627a5
Timyr4ikon <54175850+Timyr4ikon@users.noreply.github.com>
2020-11-01 03:15:30 +0200

    save back-pic

--- App.js
+++ App.js
@@ -1,21 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  ScrollView,
+  ImageBackground,
+  View,
+  Image,
+  TouchableOpacity,
+  Text,
+  Dimensions,
+} from "react-native";
 
-export default function App() {
+import { AppLoading } from "expo";
+import { useFonts, Bevan_400Regular } from "@expo-google-fonts/bevan";
+
+const screen = Dimensions.get("screen");
+
+export default () => {
+  let [fontsLoaded, error] = useFonts({
+    Bevan: Bevan_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={require("./images/background.png")}
+      style={styles.backImage}
+    >
+      <ScrollView>
+        <View style={styles.container}>
+          <TouchableOpacity style={styles.bntPlay}>
+            <Image
+              source={require("./images/kk.png")}
+              style={styles.btnPlayImg}
+            />
+            <Text style={styles.btnPlayText}>Play</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.bntPlay}>
+            <Image
+              source={require("./images/kk.png")}
+              style={styles.btnPlayImg}
+            />
+            {/* <Text style={styles.btnPlayText}></Text> */}
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </ImageBackground>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    height: screen.height,
+  },
+  backImage: {
+    flex: 1,
+    resizeMode: "cover",
+  },
+  bntPlay: {
+    borderWidth: 1,
+    borderColor: "red",
+    width: "70%",
+    alignItems: "center",
+    maxWidth: 300,
+    position: "relative",
+  },
+  btnPlayImg: {
+    width: "100%",
+    height: 75,
+    resizeMode: "stretch",
+  },
+  btnPlayText: {
+    position: "absolute",
+    color: "#ffffff",
+    fontSize: 33,
+    lineHeight: 75,
+    textTransform: "uppercase",
+    fontFamily: "Bevan",
   },
 });
